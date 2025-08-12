9c3f8f5e8412a0e59aae6131bbb39147e1dc9d96
bikdistinct <bikdev07@gmail.com>
2025-08-12 18:27:47 +0530

    auth path corrected

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const formRoutes = require("./routes/formRoutes");
-const authRoutes =require("./routes/authroutes")
+const authRoutes =require("./routes/authRoutes")
 
 dotenv.config();
 
